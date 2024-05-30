1960f970bbebad740e5fafa72a36aeb1863cefdf
hongjubrother <163494004+hongjubrother@users.noreply.github.com>
2024-05-30 13:28:21 +0900

    Update sprites.py
    def animate 추가

--- opensw/sprites.py
+++ opensw/sprites.py
@@ -21,14 +21,32 @@ class Player(pg.sprite.Sprite):
     def __init__(self,game):
         pg.sprite.Sprite.__init__(self)
         self.game = game
-        self.image = pg.Surface((30,40))
-        self.image.fill(YELLOW)
+        self.walking = False
+        self.jumping = False
+        self.current_frame = 0 # ex) 걷는 모션은 2프레임
+        self.last_update = 0 
+        self.load_images() #한번에 묶기
+        self.image = self.standing_frames[0]
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH /2 , HEIGHT /2)
         self.pos = vec(WIDTH/2, HEIGHT/2)# 객체의 위치를 나타내는 변수
         self.vel = vec(0, 0)# vel = 속도, 초기 속도 0 , 정지를 의미
         self.acc = vec(0, 0) # acc = 가속도 , 초기 가속도 0으로 설정
 
+    def load_images(self): 
+        self.standing_frames = [self.game.spritesheet.get_image(614, 1063, 120, 191), 
+                                self.game.spritesheet.get_image(690, 406, 120, 201)]
+        for frame in self.standing_frames:
+                frame.set_colorkey(BLACK)
+        self.walk_frames_r = [self.game.spritesheet.get_image(678, 860, 120,201), 
+                                self.game.spritesheet.get_image(692, 1458, 120, 207)]
+        self.walk_frames_l = []
+        for frame in self.walk_frames_r:
+            frame.set_colorkey(BLACK)
+            self.walk_frames_l.append(pg.transform.flip(frame, True, False))
+        self.jump_frame = [self.game.spritesheet.get_image(382, 763, 150, 181)]
+        self.jump_frame.set_colorkey(BLACK)
+    
     def jump(self):
         
         # jump only if standing
@@ -41,6 +59,7 @@ class Player(pg.sprite.Sprite):
 
 
     def update(self):
+        self.animate()
         self.acc = vec(0, PLAYER_GRAV) 
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]: 
@@ -61,6 +80,17 @@ class Player(pg.sprite.Sprite):
             
         self.rect.midbottom = self.pos
 
+    def animate(self): #프레임 부분(캐릭터 움직이는 애니메이션)
+        now  = pg.time.get_ticks()
+        if not self.jumping and not self.walking:
+            if now - self.last_update > 200:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.standing_frames)
+                bottom = self.rect.bottom
+                self.image = self.standing_frames[self.current_frame]
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
+                
 class Platform(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
         pg.sprite.Sprite.__init__(self)
