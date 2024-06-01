ae175ce376debd14aa383abff1d74e0e506c505b
hongjubrother <163494004+hongjubrother@users.noreply.github.com>
2024-06-01 15:43:46 +0900

    main 추가
    1. 캐릭터 애니메이션 구현(움직임)
    2. 플랫폼 그래픽 예제 추가
    3. 점프 매커니즘 추가: 짧은/긴 점프 구분)
    4. 배경음악/효과음 추가
    5. 파워업( 슈퍼점프 ) 추가
    6. 장애물 요소 추가

--- opensw/main.py
+++ opensw/main.py
@@ -1,9 +1,15 @@
+# KidsCanCode - Game Development with Pygame video series
+# Jumpy! (a platform game) - Part 12
+# Video link: https://youtu.be/qnUVjACD3WM
+# Platform Graphics
+# Art from Kenney.nl
 
-#Jumpy! - platform game
-import pygame as pg  # type: ignore
+import pygame as pg
 import random
+import time
 from settings import *
-from sprites import * 
+from sprites import *
+from os import path
 
 class Game:
     def __init__(self):
@@ -14,53 +20,112 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
-        
+        self.font_name = path.join(path.dirname(__file__), FONT_NAME)
+        self.load_data()
+
+    def load_data(self):
+        # load high score
+        self.dir = path.dirname(__file__)
+        img_dir = path.join(self.dir, 'img')
+        with open(path.join(self.dir, HS_FILE), 'r') as f:
+            try:
+                self.highscore = int(f.read())
+            except:
+                self.highscore = 0
+        # load spritesheet image
+        self.spritesheet = Spritesheet(path.join(img_dir, SPRITESHEET))
+        # load sounds
+        self.snd_dir = path.join(self.dir, 'snd')
+        self.jump_sound = pg.mixer.Sound(path.join(self.snd_dir, 'Kirby_jump1.mp3'))
+        self.boost_sound = pg.mixer.Sound(path.join(self.snd_dir, 'powerup.mp3')) 
+
+
     def new(self):
-        #reset game(게임 죽으면 다시 시작하는거)
-        self.all_sprites = pg.sprite.Group()
+        # start a new game
+        self.score = 0
+        self.all_sprites = pg.sprite.LayeredUpdates() 
         self.platforms = pg.sprite.Group()
+        self.powerups = pg.sprite.Group()
+        self.mobs = pg.sprite.Group()
         self.player = Player(self)
-        self.all_sprites.add(self.player)
         for plat in PLATFORM_LIST:
-            p = Platform(*plat)
-            self.all_sprites.add(p)
-            self.platforms.add(p)
-
-        '''
-        p1 = Platform(0,HEIGHT -40, WIDTH, 40)
-        self.all_sprites.add(p1)
-        self.platforms.add(p1)
-        p2 = Platform(WIDTH/2 -50, HEIGHT * 3 /4 , 100, 20)
-        self.all_sprites.add(p2) 
-        self.platforms.add(p2)
-        '''
-
+            Platform(self, *plat)
+        self.mob_timer = 0
+        pg.mixer.music.load(path.join(self.snd_dir, 'Kirby_bgm1.mp3'))
         self.run()
 
     def run(self):
-        #Game loop
+        # Game Loop
+        pg.mixer.music.play(loops=-1)
         self.playing = True
         while self.playing:
             self.clock.tick(FPS)
             self.events()
             self.update()
-            self.update()
             self.draw()
-
+        pg.mixer.music.fadeout(1000)
     def update(self):
-        #Game loop - update
+        # Game Loop - Update
         self.all_sprites.update()
+
+        # 몹 소환
+        now = pg.time.get_ticks()
+        if now - self.mob_timer > 5000 + random.choice([-1000, -500, 0, 500, 1000]):
+            self.mob_timer = now
+            Mob(self)
         # check if player hits a platform - only if falling
         if self.player.vel.y > 0:
             hits = pg.sprite.spritecollide(self.player, self.platforms, False)
             if hits:
-                self.player.pos.y = hits[0].rect.top
-                self.player.vel.y = 0
+                lowest = hits[0]
+                for hit in hits:
+                    if hit.rect.bottom > lowest.rect.bottom:
+                        lowest = hit
+                # 커비 이미지 파일에 따라 새로 고쳐야됨(발 platform 에 닫는 코드)        
+                if self.player.pos.x < lowest.rect.right + 10 and self.player.pos.x > lowest.rect.left + 10:
+                    if self.player.pos.y < lowest.rect.centery:
+                        self.player.pos.y = lowest.rect.top
+                        self.player.vel.y = 0
+                        self.player.jumping = False
+
+        # if player reaches top 1/4 of screen
+        if self.player.rect.top <= HEIGHT / 4:
+            self.player.pos.y += max(abs(self.player.vel.y), 2)
+            for mob in self.mobs:
+                mob.rect.y += max(abs(self.player.vel.y), 2)
+            for plat in self.platforms:
+                plat.rect.y += max(abs(self.player.vel.y), 2)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+                    self.score += 10
+        # if player hits powerup
+        pow_hits = pg.sprite.spritecollide(self.player, self.powerups, True)
+        for pow in pow_hits:
+            if pow.type == 'boost':
+                self.boost_sound.play()
+                self.player.vel.y = -BOOST_POWER
+                self.player.jummping = False
+
+        # Die!
+        if self.player.rect.bottom > HEIGHT:
+            for sprite in self.all_sprites:
+                sprite.rect.y -= max(self.player.vel.y, 10)
+                if sprite.rect.bottom < 0:
+                    sprite.kill()
+        if len(self.platforms) == 0:
+            self.playing = False
+
+        # spawn new platforms to keep same average number
+        while len(self.platforms) < 6:
+            width = random.randrange(50, 100)
+            Platform(self, random.randrange(0, WIDTH - width),
+                         random.randrange(-75, -30))
+            
 
     def events(self):
-        #Game loop - events
+        # Game Loop - events
         for event in pg.event.get():
-            #check for closing window
+            # check for closing window
             if event.type == pg.QUIT:
                 if self.playing:
                     self.playing = False
@@ -68,32 +133,76 @@ class Game:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
                     self.player.jump()
-
+            if event.type == pg.KEYUP:
+                if event.key == pg.K_SPACE:
+                    self.player.jump_cut()
 
     def draw(self):
-        #Game loop - draw
-        self.screen.fill(BLACK)
+        # Game Loop - draw
+        self.screen.fill(BGCOLOR)
         self.all_sprites.draw(self.screen)
-
-        #after drawing everything, filp the dispaly
-        pg.display.flip( )
+        self.draw_text(str(self.score), 22, WHITE, WIDTH / 2, 15)
+        # *after* drawing everything, flip the display
+        pg.display.flip()
 
     def show_start_screen(self):
-        #game splash / start screen
-        pass
+        # game splash/start screen
+        pg.mixer.music.load(path.join(self.snd_dir, 'Kirby_opening_bgm.mp3'))
+        pg.mixer.music.play(loops=-1)
+        self.screen.fill(BGCOLOR)
+        self.draw_text(TITLE, 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("Arrows to move, Space to jump", 22, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("Press a key to play", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        self.draw_text("High Score: " + str(self.highscore), 22, WHITE, WIDTH / 2, 15)
+        pg.display.flip()
+        self.wait_for_key()
+        pg.mixer.music.fadeout(500)
 
     def show_go_screen(self):
-        #game over/continue
-        pass
+        # game over/continue
+        if not self.running:
+            return
+        pg.mixer.music.load(path.join(self.snd_dir, 'Kirby_bgm3.mp3'))
+        self.screen.fill(BGCOLOR)
+        self.draw_text("GAME OVER", 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("Score: " + str(self.score), 22, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("Press a key to play again", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.draw_text("NEW HIGH SCORE!", 22, WHITE, WIDTH / 2, HEIGHT / 2 + 40)
+            with open(path.join(self.dir, HS_FILE), 'w') as f:
+                f.write(str(self.score))
+        else:
+            self.draw_text("High Score: " + str(self.highscore), 22, WHITE, WIDTH / 2, HEIGHT / 2 + 40)
+        pg.display.flip()
+        pg.mixer.music.play(0, 0.0)
+        time.sleep(3)
+        pg.mixer.music.stop() 
+        pg.mixer.music.fadeout(500)
+        self.wait_for_key()
 
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.running = False
+                if event.type == pg.KEYUP:
+                    waiting = False
 
-g =  Game()
+    def draw_text(self, text, size, color, x, y):
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
+
+g = Game()
 g.show_start_screen()
 while g.running:
     g.new()
     g.show_go_screen()
 
 pg.quit()
-
-아아아아
-
