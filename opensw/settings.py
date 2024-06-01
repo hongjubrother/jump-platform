73aef3e94fee550404d726a31e3baa24c6e2ef95
hongjubrother <163494004+hongjubrother@users.noreply.github.com>
2024-06-01 15:45:28 +0900

    Update settings.py

--- opensw/settings.py
+++ opensw/settings.py
@@ -1,21 +1,33 @@
-
-TITLE = " 새싹 레이스 : 하늘을 향한 모험"
-# game option  / settings
+# game options/settings
+TITLE = "새싹, 하늘을 향한 레이스"
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
+FONT_NAME = 'NanumBarunGothicBold.ttf'
+HS_FILE = "highscore.txt"
+SPRITESHEET = "spritesheet_jumper.png"
 
-# Player properties 속도 조절 
+# Player properties
 PLAYER_ACC = 0.5
 PLAYER_FRICTION = -0.12
 PLAYER_GRAV = 0.8
+PLAYER_JUMP = 20
 
-# Starting Platforms
-PLATFORM_LIST = [(0, HEIGHT -40, WIDTH, 40),
-                 (WIDTH /2 -50, HEIGHT *3/4, 100, 20),
-                 (125, HEIGHT- 350, 100, 20),
-                 (350, 200, 100, 20),
-                 (175, 100, 50, 20)]
+# Game properties
+BOOST_POWER = 60
+POW_SPAWN_PCT = 7
+MOB_FREQ = 5000
+PLAYER_LAYER = 2
+PLATFORM_LAYER = 1
+POW_LAYER = 1
+MOB_LAYER = 2
+
+# Starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 60),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4 - 50),
+                 (125, HEIGHT - 350),
+                 (350, 200),
+                 (175, 100)]
 
 # define colors
 WHITE = (255, 255, 255)
@@ -25,6 +37,4 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 LIGHTBLUE = (0, 155, 155)
-
-
-
+BGCOLOR = LIGHTBLUE
